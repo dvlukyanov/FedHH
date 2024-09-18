3dbe426ac9980e5d5de5c6ea7239fa8b5fc91553
Dmitry Lukyanov <dmitry.v.lukyanov@gmail.com>
2024-09-18 16:07:25 -0400

    intermediate commit

--- architectures/shared/logger.py
+++ architectures/shared/logger.py
@@ -3,7 +3,6 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
 from architectures.shared.config import Config
-from architectures.shared.synchronized import synchronized
 
 
 __author__ = 'Dmitry Lukyanov'
@@ -16,23 +15,18 @@ class Logger():
     def __init__(self):
         self.folder = Config()['log']['folder']
 
-    @synchronized
     def server(self, msg):
         self.__log('server.log', msg)
 
-    @synchronized
     def edge(self, msg):
         self.__log('edge.log', msg)
 
-    @synchronized
     def client(self, msg):
         self.__log('client.log', msg)
 
-    @synchronized
     def proxy(self, msg):
         self.__log('proxy.log', msg)
 
-    @synchronized
     def worker(self, msg):
         self.__log('worker.log', msg)
 
